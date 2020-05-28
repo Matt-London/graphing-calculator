b35da2b089cf2ac2fd4f6532e244bd7d37c5326b
Matt London <mattlondon57@gmail.com>
2020-05-28 00:23:10 -0400

    Tests for Linear and Quadratic graph

--- Main.cpp
+++ Main.cpp
@@ -12,8 +12,7 @@ using namespace std;
 
 int main() {
     Cartesian test(30, 60);
-    for(int i = -14; i < 30; i++) {
-        test.plot((double) i, (double) pow(i, 2));
-    }
+    Linear lin(-5, 0);
+    test.plot(lin);
     test.print_graph();
 }
