018490b3c7e0ff93617c1927e4fe0e6ee9676a01
Matt London <mattlondon57@gmail.com>
2020-06-18 14:28:28 -0400

    Move headers

---
+++ include/objects/Function.hpp
@@ -0,0 +1,16 @@
+#ifndef FUNCTION_GUARD
+#define FUNCTION_GUARD
+
+class Function {
+    public:
+        // ================ Constructors ================
+        Function();
+
+        // ================ Methods ================
+        double trace(double x);
+        double *solve(double y);
+        void print_equation();
+
+};
+
+#endif
