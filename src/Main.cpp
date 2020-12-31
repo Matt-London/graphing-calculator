dd890d22b8882e2298bf5c41ee8ba855c6c625a7
Matt London <mattlondon57@gmail.com>
2020-12-31 16:38:40 -0500

    Update example to quadratic

--- src/Main.cpp
+++ src/Main.cpp
@@ -11,13 +11,12 @@
 using namespace std;
 
 int main() {
+    // a, b, c values for simple cubic functions
+    // Graphing y = (1/2)*x^2
     vector<double> v;
-    v.push_back(0);
-    v.push_back(0);
-    v.push_back(0);
-    v.push_back(0);
-
-    v.push_back(0.00001);
+    v.push_back(0); // c
+    v.push_back(0); // b
+    v.push_back(0.25); // a
 
 
     Polynomial q(v);
